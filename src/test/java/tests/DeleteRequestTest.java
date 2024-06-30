d9f2e8f317fa2544b88e6f7523b36f80275f8fed
ARVINDMS544 <arvindsudhi44@gmail.com>
2024-06-30 18:05:16 +0530

    Commit 1

---
+++ src/test/java/tests/DeleteRequestTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class DeleteRequestTest {
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void deleteRequest() {
+        Response response = given()
+                .header("Content-type", "application/json")
+                .when()
+                .delete("/posts/1")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        System.out.println("DELETE REQUEST PASSED");
+    }
+}
